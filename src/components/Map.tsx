47d905ac8f9e1178ee871142851ca6f29ae68417
thunder-volt <ishu114407@gmail.com>
2023-01-05 15:22:33 +0530

    points connected

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -6,7 +6,9 @@ import '../styles/MapDisplay.css'
 import MapBoxGeocoder from '@mapbox/mapbox-gl-geocoder'
 import '../styles/Marker.css'
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
+
 const Map = () => {
+  // var MapboxDirections = require('@mapbox/mapbox-gl-directions');
   const map: any = useRef(null);
   const mapContainer = useRef<HTMLDivElement>(null);
   const [lat, setLat] = useState(77.1025);
@@ -17,7 +19,7 @@ const Map = () => {
     if (map.current) return
     map.current = new mapboxgl.Map({
       container: mapContainer.current || '',
-      style: 'mapbox://styles/mapbox/streets-v11',
+      style: 'mapbox://styles/mapbox/streets-v12',
       center: [lat, lng],
       zoom: zoom,
     })
@@ -36,6 +38,93 @@ const Map = () => {
     map.current.addControl(search, 'top-right')
     map.current.addControl(new mapboxgl.NavigationControl())
 
+    map.current.on('load', () => {
+      map.current.addSource('route', {
+        'type': 'geojson',
+        'data': {
+          'type': 'Feature',
+          'properties': {},
+          'geometry': {
+            'type': 'LineString',
+            'coordinates': [
+              [-122.483696, 37.833818],
+              [-122.483482, 37.833174],
+              [-122.483396, 37.8327],
+              [-122.483568, 37.832056],
+              [-122.48404, 37.831141],
+              [-122.48404, 37.830497],
+              [-122.483482, 37.82992],
+              [-122.483568, 37.829548],
+              [-122.48507, 37.829446],
+              [-122.4861, 37.828802],
+              [-122.486958, 37.82931],
+              [-122.487001, 37.830802],
+              [-122.487516, 37.831683],
+              [-122.488031, 37.832158],
+              [-122.488889, 37.832971],
+              [-122.489876, 37.832632],
+              [-122.490434, 37.832937],
+              [-122.49125, 37.832429],
+              [-122.491636, 37.832564],
+              [-122.492237, 37.833378],
+              [-122.493782, 37.833683]
+            ]
+          }
+        }
+      });
+      map.current.addLayer({
+        'id': 'route',
+        'type': 'line',
+        'source': 'route',
+        'layout': {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        'paint': {
+          'line-color': '#011F5B',
+          'line-width': 8
+        }
+      });
+    });
+
+    // // San Francisco
+    // var origin = [-122.414, 37.776];
+
+    // // Washington DC
+    // var destination = [-77.032, 38.913];
+
+    // // A simple line from origin to destination.
+    // var route = {
+    //   'type': 'FeatureCollection',
+    //   'features': [
+    //     {
+    //       'type': 'Feature',
+    //       'geometry': {
+    //         'type': 'LineString',
+    //         'coordinates': [origin, destination]
+    //       }
+    //     }
+    //   ]
+    // };
+
+    // map.current.on('load', function () {
+    //   // Add a source and layer displaying a point which will be animated in a circle.
+    //   map.current.addSource('route', {
+    //     'type': 'geojson',
+    //     'data': route
+    //   });
+
+    //   map.current.addLayer({
+    //     'id': 'route',
+    //     'source': 'route',
+    //     'type': 'line',
+    //     'paint': {
+    //       'line-width': 2,
+    //       'line-color': '#007cbf'
+    //     }
+    //   });
+
+    // });
   })
   return (
     <>
