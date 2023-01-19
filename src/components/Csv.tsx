e249786893cf3a322da0e8af5c10d300e9906beb
Jayanth <jayanth151002@gmail.com>
2023-01-20 00:38:55 +0530

    Read and parse excel into json in frontend for new orders

--- src/components/Csv.tsx
+++ src/components/Csv.tsx
@@ -1,34 +1,39 @@
-import React from 'react'
+import * as XLSX from 'xlsx'
 import "../styles/Csv.css"
 const Csv = () => {
+
+    const readExcel = async (e: any) => {
+        const file = e.target.files[0];
+        const data = await file.arrayBuffer();
+        const workbook = XLSX.read(data);
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+        const jsonData = XLSX.utils.sheet_to_json(worksheet);
+        return new Promise((resolve, reject) => {
+            resolve(jsonData)
+        })
+    }
+
     return (
         <div>
             <input type="file" onChange={(e) => {
-                // e.preventDefault()
 
-                // let file = e.target.files[0]
-
-                // let formData = new FormData()
-                // formData.append('file', file)
-                // console.log(formData)
-                // fetch('http://localhost:8000/route/new', {
-                //     method: 'POST',
-                //     body: formData,
-                //     headers: {
-                //         "Content-Type": "multipart/form-data",
-                //         "Accept": "application/json",
-                //         "type": "formData"
-                //     },
-                // })
-                //     .then(resp => resp.json())
-                //     .then(data => {
-                //         if (data.errors) {
-                //             alert(data.errors)
-                //         }
-                //         else {
-                //             console.log(data)
-                //         }
-                //     })
+                e.preventDefault()
+                readExcel(e)
+                    .then((data: any) => {
+                        fetch('http://localhost:8000/orders/new', {
+                            method: 'POST',
+                            body: JSON.stringify(data),
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                        })
+                            .then(resp => resp.json())
+                            .then(data => {
+                                console.log(data)
+                            })
+                            .catch(err => console.log(err))
+                    })
+                    .catch(err => console.log(err))
             }
             }
 
