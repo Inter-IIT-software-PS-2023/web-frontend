29e0869d43416c6e9d7e306b1a52c580fe67f455
thunder-volt <ishu114407@gmail.com>
2023-02-02 03:11:14 +0530

    token updated

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -36,7 +36,7 @@ const Map = () => {
 		setOpen(!open)
 	}
 	const setMarkers = (Riders = riders) => {
-		mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN
+		mapboxgl.accessToken = "pk.eyJ1IjoiaXNodTExNDQwNyIsImEiOiJjbGNpcHdqdjkwMnplM29xbXJjdXRoM3hiIn0.7bDoT4N8RAglxqUzf8lKvg"
 		map.current = new mapboxgl.Map({
 			container: mapContainer.current || '',
 			style: theme,
