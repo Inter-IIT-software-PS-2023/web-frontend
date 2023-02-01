44db06a9725a479543d23b90320016e088d01a6c
thunder-volt <ishu114407@gmail.com>
2023-02-02 03:13:15 +0530

    token updated

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useRef, useState, useEffect } from 'react'
+import { useRef, useState, useEffect } from 'react';
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
