a81b6de5cfd7858f3ec7804921ef1ab89f436650
thunder-volt <ishu114407@gmail.com>
2023-02-05 23:10:24 +0530

    css fixed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box'
 import Stepper from '@mui/material/Stepper'
 import Step from '@mui/material/Step'
 import StepLabel from '@mui/material/StepLabel'
-import StepContent from '@mui/material/StepContent'
+import StepContent from '@mui/material/StepContent';
 import Button from '@mui/material/Button'
 import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
